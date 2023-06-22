b739df2edc3a3ef699f3a806908e65b9abd805ce
Puwanai Sangphon <puwanai_san@dsi.go.th>
2023-06-22 14:01:32 +0700

    update word save to server

--- src/DocTemplate.php
+++ src/DocTemplate.php
@@ -9,8 +9,41 @@ class DocTemplate
         // 
     }
 
-    public static function getData()
+
+
+    public static function generate($file, $data = [])
     {
-        return 'MyData';
+        $templateProcessor = new TemplateProcessor($file);
+        foreach ($data as $key => $value) {
+            $val = self::clean_text($value);
+            if (is_array($val) && count($val) && $key == 'detail') {
+                print_r($val);
+                $templateProcessor->cloneBlock($key, 0, true, false, $val);
+            } else {
+                $templateProcessor->setValue($key, $val);
+            }
+        }
+        return $templateProcessor;
+    }
+
+    public static function clean_text($text)
+    {
+        $tmp = self::count_tag($text);
+
+        if (count($tmp)) {
+            $return = [];
+            foreach ($tmp as $t) {
+                $return[] = ['de' => strip_tags($t)];
+            }
+            return $return;
+        }
+
+        return $text;
+    }
+
+    public static function count_tag($contents)
+    {
+        preg_match_all("#<p[^>]*>(.*?)</p>#i", $contents, $matches);
+        return $matches[1];
     }
 }

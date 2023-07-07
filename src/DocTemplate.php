693951e19759cb21abfa41c57a89f6ccfd9eec25
bowswoy <puwanai.sangphon@gmail.com>
2023-07-07 13:33:20 +0700

    Update DocTemplate.php

--- src/DocTemplate.php
+++ src/DocTemplate.php
@@ -8,7 +8,7 @@ class DocTemplate
 {
     function __construct()
     {
-        // 
+        //
     }
 
     public static function save($file, $data = [], $filename = 'ps-word-save.docx')
@@ -25,7 +25,17 @@ class DocTemplate
             if (is_array($val) && count($val) && $key == 'detail') {
                 $templateProcessor->cloneBlock($key, 0, true, false, $val);
             } else {
-                $templateProcessor->setValue($key, $val);
+                $val = self::string_to_array($value);
+                if (is_array($val) && count($val)) {
+                    $templateProcessor->cloneBlock($key, 0, true, false, $val);
+                } else {
+                    $find = strpos($val, ';base64,');
+                    if ($find !== false) {
+                        $templateProcessor->setImageValue($key, $val);
+                    } else {
+                        $templateProcessor->setValue($key, $val);
+                    }
+                }
             }
         }
         return $templateProcessor;
@@ -37,7 +47,7 @@ class DocTemplate
         if (count($tmp)) {
             $return = [];
             foreach ($tmp as $t) {
-                $return[] = ['de' => strip_tags($t)];
+                $return[] = ['txt' => strip_tags($t)];
             }
             return $return;
         }
@@ -48,5 +58,23 @@ class DocTemplate
     {
         preg_match_all("#<p[^>]*>(.*?)</p>#i", $contents, $matches);
         return $matches[1];
+    }
+
+    public static function string_to_array($contents)
+    {
+        $text = [];
+        $sp_tmp  = nl2br($contents);
+        $sp_tmp = explode("<br />", $sp_tmp);
+        if (count($sp_tmp) > 1) {
+            foreach ($sp_tmp as $sp) {
+                if ($sp) {
+                    $text[] = ['txt' => $sp];
+                }
+            }
+        } else {
+            $text = $contents;
+        }
+
+        return $text;
     }
 }
